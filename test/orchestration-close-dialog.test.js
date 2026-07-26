4cc21cf5e1c0a22c51b2a7a3c40ff8b247b67459
bojantv <58706872+bojantv@users.noreply.github.com>
2026-07-26 16:52:38 +0200

    refactor: remove secondary confirmation action

--- test/orchestration-close-dialog.test.js
+++ test/orchestration-close-dialog.test.js
@@ -5,9 +5,13 @@ var path = require("node:path");
 
 test("orchestration close confirmation presents two distinct choices", function () {
   var source = fs.readFileSync(path.join(__dirname, "../lib/public/modules/queued-messages.js"), "utf8");
+  var html = fs.readFileSync(path.join(__dirname, "../lib/public/index.html"), "utf8");
+  var modalSource = fs.readFileSync(path.join(__dirname, "../lib/public/modules/app-misc.js"), "utf8");
   var closeTaskStart = source.indexOf("function closeTask(task)");
   var closeTaskEnd = source.indexOf("\n}\n\nexport function", closeTaskStart);
   var closeTaskSource = source.slice(closeTaskStart, closeTaskEnd);
 
-  assert.match(closeTaskSource, /"Close task",\s*true,\s*null,\s*null,\s*"Keep task"/);
+  assert.match(closeTaskSource, /"Close task",\s*true,\s*"Keep task"/);
+  assert.doesNotMatch(html, /confirm-secondary/);
+  assert.doesNotMatch(modalSource, /confirmSecondary|secondaryLabel|onSecondary/);
 });
